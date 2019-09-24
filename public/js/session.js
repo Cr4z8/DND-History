0dc6a812c40d7f8977ad197c0d05a61054dd156c
ucwwt <ucwwt@student.kit.edu>
2019-09-24 17:47:04 +0200

    implemented minimize and maximize functions for the text body

--- public/js/session.js
+++ public/js/session.js
@@ -1,63 +1,93 @@
 const cardContainer = document.getElementById("adventure-card-container");
+var minmax = "";
 
 document.getElementById("adder-circle").addEventListener("click", () => {
     const card = document.createElement('div');
     card.className = "adventure-card";
-    const editableFace = createEditableFace(card);
+    const date = new Date();
+    const dateString = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
+    const editableFace = createEditableFace(card, "", dateString);
     card.appendChild(editableFace);
     cardContainer.appendChild(card);
     card.firstElementChild.firstElementChild.focus();
 });
 
 function createEditableFace(card, headerText, date, bodyText) {
-    const htmlEditable = document.getElementById("editable-face-template").innerHTML;
-    const editableFace = document.createElement("div");
-    editableFace.className = "face";
-    editableFace.innerHTML = htmlEditable;
-    //input-text
+    const editableFace = createFaceFromTemplate("editable-face-template", "face");
+    //header-input-text
     const headerInput = editableFace.firstElementChild;
     const bodyInput = editableFace.children[2];
     headerInput.value = headerText == null ? "" : headerText;
-    headerInput.addEventListener("keypress", (e) => {
-        if (e.which == 13) headerInput.blur();
-    });
-    headerInput.addEventListener("blur", () => {
-        //todo: falls focus auf header oder body input liegt, dann nicht wechseln
-        const normalFace = createNormalFace(card, headerInput.value, date, bodyInput.value);
-        card.removeChild(editableFace);
-        card.appendChild(normalFace);
-        scaleFontSize(normalFace.firstElementChild);
-        scaleContainerSize(normalFace.children[2]);
-    });
+    bodyInput.value = bodyText == null ? "" : bodyText;
+    addBlurEventListener(headerInput, card, editableFace, headerInput, date, bodyInput);
     //date
     editableFace.children[1].innerHTML = date == null ? "" : date;
-    //body-text
+    //body-input-text
+    addBlurEventListener(bodyInput, card, editableFace, headerInput, date, bodyInput);
     return editableFace;
 }
 
 function createNormalFace(card, headerText, date, bodyText) {
-    const normalFace = document.createElement("div");
-    normalFace.className = "face";
-    const template = document.getElementById("face-template").innerHTML;
-    const htmlNormal = Mustache.render(template, {
+    const normalFace = createFaceFromTemplate("face-template", "face", {
         headerText: headerText,
         date: date,
-        bodyText: bodyText
+        bodyText: bodyText,
+        minmax: minmax
     });
-    normalFace.innerHTML = htmlNormal;
-    //editsymbol
+    //minimize
     normalFace.children[3].addEventListener("click", () => {
+        if (minmax == "verkleinern") {
+            normalFace.children[2].style.height = "150px";
+            minmax = "vergrößern";
+        } else if (minmax == "vergrößern") {
+            scaleContainerSize(normalFace.children[2]);
+            minmax = "verkleinern";
+        }
+        normalFace.children[3].innerHTML = minmax;
+    });
+    //editsymbol
+    normalFace.children[4].addEventListener("click", () => {
         const editableFace = createEditableFace(card, headerText, date, bodyText);
         card.removeChild(normalFace);
         card.appendChild(editableFace);
-        card.firstElementChild.firstElementChild.focus();
+        editableFace.children[2].focus();
+        scaleContainerSize(editableFace.children[2]);
     })
     return normalFace;
 }
 
+function addBlurEventListener(container, card, editableFace, headerInput, date, bodyInput) {
+    container.addEventListener("keypress", (e) => {
+        if (e.which == 13) container.blur();
+    });
+    container.addEventListener("blur", () => {
+        setTimeout(() => {
+            if (document.activeElement == editableFace.children[0] || document.activeElement == editableFace.children[2]) {
+                return;
+            }
+            minmax = editableFace.children[2].scrollHeight > 200 ? "verkleinern" : "";
+            const normalFace = createNormalFace(card, headerInput.value, date, bodyInput.value);
+            card.removeChild(editableFace);
+            card.appendChild(normalFace);
+            scaleFontSize(normalFace.firstElementChild);
+            scaleContainerSize(normalFace.children[2]);
+        }, 50);
+    });
+}
+
+
+function createFaceFromTemplate(templateID, faceClassName, mustacheObject) {
+    const template = document.getElementById(templateID).innerHTML;
+    const face = document.createElement("div");
+    face.className = faceClassName;
+    const html = mustacheObject == null ? template : Mustache.render(template, mustacheObject);
+    face.innerHTML = html;
+    return face;
+}
+
+
 function scaleFontSize(container) {
     let fontSize = 36;
-    console.log(fontSize);
     while (container.scrollHeight > container.clientHeight && fontSize > 5) {
         fontSize = 0.95 * fontSize;
         container.style.fontSize = fontSize + "px";
@@ -66,6 +96,6 @@ function scaleFontSize(container) {
 
 function scaleContainerSize(container) {
     if (container.scrollHeight > container.clientHeight) {
-        container.clientHeight = container.scrollHeight;
+        container.style.height = container.scrollHeight + "px";
     }
 }
